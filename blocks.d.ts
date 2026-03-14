a728387ccd6ce97b9a6fc932bfdfa3bd1c4c9712
a012 <Aeagan0507@gmail.com>
2026-03-13 20:23:05 -0400

    Create blocks.d.ts

---
+++ blocks.d.ts
@@ -0,0 +1,13 @@
+declare namespace control {
+  //% help=github:pxt-hardware-programming-docs/docs/_onCodeStart
+  function _onCodeStart(args: any): void;
+   //% help=github:pxt-hardware-programming-docs/docs/_onCodeStop
+  function _onCodeStop(args: any): void;
+}
+
+//% help=github:pxt-hardware-programming-docs/docs/buffers
+declare namespace Buffer {
+  
+}
+
+// other code supported in the near future
